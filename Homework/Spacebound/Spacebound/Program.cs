d9e9b2b6c428b25c1f22082dfc2afa847393eca3
dilan-patel <dpatel@spartaglobal.com>
2019-11-04 22:47:01 +0000

    Homework Week3 Day1
    Added game loop and used exception handling for invalid input in game loop.

---
+++ Homework/Spacebound/Spacebound/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace Spacebound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to Spacebound!");
+            Game spacebound = new Game();
+            spacebound.GameLoop();
+        }
+
+        public class Game
+        {
+            private int level;
+            private string retry;
+            private string playGame;
+            private string playerName;
+
+            public Game()
+            {
+
+            }
+
+            public string getUserInput()
+            {
+                return Console.ReadLine();
+            }
+
+            public void GameLoop()
+            {
+                while (true)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Would you like to start (Type 'yes' or 'no'): ");
+                    playGame = getUserInput().ToLower();
+
+                    try
+                    {
+                        if (playGame == "yes")
+                        {
+                            Console.WriteLine("The game is now playing.");
+                        }
+                        else if (playGame == "no")
+                        {
+                            Console.WriteLine("Thank you for playing.");
+                            break;
+                        }
+                        else
+                        {
+                            throw new InvalidInputException("Your input was invalid. Please type 'yes' or 'no' only.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+
+    [Serializable]
+    internal class InvalidInputException : Exception
+    {
+        public InvalidInputException()
+        {
+        }
+
+        public InvalidInputException(string message) : base(message)
+        {
+        }
+
+        public InvalidInputException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidInputException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
